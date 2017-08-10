31c8ea2ec16af47361f52ed61dfbf5de2cbfbe38
sakthibharathi <30821016+sakthibharathi@users.noreply.github.com>
2017-08-10 22:14:37 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int n1, n2, n3;
+ 
+    printf("Enter the values of n1, n2 and n3\n");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    printf("n1 = %d\tn2 = %d\tn3 = %d\n", n1, n2, n3);
+    if (n1 > n2)
+    {
+        if (n1 > n3)
+        {
+            printf("n1 is the greatest among three \n");
+        }
+        else
+        {
+            printf("n3 is the greatest among three \n");
+        }
+    }
+    else if (n2 > n3)
+        printf("n2 is the greatest among three \n");
+    else
+        printf("n3 is the greatest among three \n");
+}
