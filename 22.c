f5248b36eff655dd2f6bbc9b29c132584335ba42
sakthibharathi <30821016+sakthibharathi@users.noreply.github.com>
2017-08-10 12:42:32 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, j, gcd;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    for(j=1; j <= n1 && j <= n2; ++j)
+    {
+       
+        if(n1%j==0 && n2%j==0)
+            gcd = j;
+    }
+
+    printf("G.C.D of %d and %d is %d", n1, n2, gcd);
+
+    return 0;
+}
