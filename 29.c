4722ad41e46d9b87b8db5e22b9d60f3f09ee991c
sakthibharathi <30821016+sakthibharathi@users.noreply.github.com>
2017-08-10 04:44:54 -0700

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, k, sum = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(k=1; k <= n; ++k)
+    {
+        sum += i;   
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
+
