1c7ffc291b8bee4b7c1b1167813384b95fa04366
sakthibharathi <30821016+sakthibharathi@users.noreply.github.com>
2017-08-15 15:41:16 +0530

    Update 59.c

--- 59.c
+++ 59.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <string.h>
- 
 void main()
 {
-    char s[200];
-    int count = 0, k;
+    char s[500];
+    int c = 0, k;
  
     printf("enter the string\n");
     scanf("%[^\n]s", s);
     for (k = 0;s[k] != '\0';k++)
     {
         if (s[k] == ' ')
-            count++;    
+            c++;    
     }
-    printf("number of words in given string are: %d\n", count + 1);
+    printf("number of words in given string are: %d\n", c + 1);
     return 0;
 }
