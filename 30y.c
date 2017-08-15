036de2d9904d5a3836d25415a261abcaf246ee0c
sakthibharathi <30821016+sakthibharathi@users.noreply.github.com>
2017-08-15 16:23:36 +0530

    Create 30y.c

---
+++ 30y.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ void main()
+{
+    int k, n, s = 0;
+ 
+    printf("Enter an integer number \n");
+    scanf ("%d", &n);
+    for (k = 1; k <= n; k++)
+    {
+        s = s + i;
+    }
+    printf ("Sum of first %d natural numbers = %d\n", n, s);
+    return 0;
+}
